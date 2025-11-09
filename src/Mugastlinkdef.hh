2702e7f2a3243aaa2bebdfde4d7aaa9b8e88e95b
Prabhat Sharma (PS) <ps143991f@gmail.com>
2025-11-09 18:35:14 +0100

    slight changes

--- src/Mugastlinkdef.hh
+++ src/Mugastlinkdef.hh
@@ -2,6 +2,6 @@
 #pragma link off all globals;
 #pragma link off all classes;
 #pragma link off all functions;
-#pragma link C++ class must2::Must2Data;
-#pragma link C++ class must2::Must2Physics;
+#pragma link C++ class mugast::MugastData;
+#pragma link C++ class mugast::MugastPhysics;
 #endif
